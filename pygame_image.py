bad05887bf16eccd49be19cd5882029e2963587a
nayuta <c0a23103a5@edu.teu.ac.jp>
2024-09-24 16:25:36 +0900

    q8

--- pygame_image.py
+++ pygame_image.py
@@ -9,16 +9,32 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img_f = pg.transform.flip(bg_img, True, False)#練習7
     pl_img = pg.image.load("fig/3.png")#練習2
     pl_img = pg.transform.flip(pl_img, True, False)#練習2
+    pl_rct = pl_img.get_rect()#練習8-1　rectオブジェクトの取得
+    pl_rct.center = (300, 200)#練習8-2　rectを使った初期座標の設定
+
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        #キーボード入力
+        key_lst = pg.key.get_pressed()#練習8-2　キーリストの取得
+        if key_lst[pg.K_UP]:
+            pl_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            pl_rct.move_ip((0,1))
+        if key_lst[pg.K_LEFT]:
+            pl_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            pl_rct.move_ip((1, 0))
+
+        #背景移動
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])#練習6
         screen.blit(bg_img_f, [x+1600, 0])#練習7
@@ -26,7 +42,7 @@ def main():
         screen.blit(bg_img, [x+3200, 0])#練習7
         screen.blit(bg_img_f, [x+4800, 0])#練習7
 
-        screen.blit(pl_img, [300, 200]) #練習4
+        screen.blit(pl_img, pl_rct) #練習4
         
         pg.display.update()
         tmr += 1        
